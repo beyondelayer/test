12e4dcb0277f4ff863d71fda94751252f34c5494
beyondelayer <150281366+beyondelayer@users.noreply.github.com>
2023-11-08 20:34:53 +0300

    Update UserController.php

--- UserController.php
+++ UserController.php
@@ -1,21 +1,5 @@
 <?php
 
-namespace App\Http\Controllers\Admin\User;
-
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\User\StoreUserRequest;
-use App\Http\Requests\Admin\User\UpdateUserRequest;
-use App\Models\City;
-use App\Models\Country;
-use App\Models\User;
-use App\Models\UserDetail;
-use App\Services\User\UserService;
-use App\Traits\UploadController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
-use Spatie\Permission\Models\Role;
-
 class UserController extends Controller
 {
     use UploadController;
@@ -25,29 +9,12 @@ class UserController extends Controller
     )
     {}
 
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $users = $this->service->getAll();
         return view('admin.template.user.user-list', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        $roles = Role::all();
-        $countries = Country::all();
-        $cities = City::all();
-        return view('admin.template.user.add-user', compact('roles', 'countries', 'cities'));
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(StoreUserRequest $request)
     {
         $userData = $request->only(['name', 'username', 'email', 'password', 'status', 'email_verified_at']);
@@ -70,17 +37,5 @@ class UserController extends Controller
         }
 
         return redirect()->back()->with('success', 'Kullanıcı başarıyla oluşturuldu');
-    }
-
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        $roles = Role::all();
-        $countries = Country::all();
-        $cities = City::all();
-        return view('admin.template.user.edit-user', compact('user', 'roles', 'countries', 'cities'));
     }
 }
